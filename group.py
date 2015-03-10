d24357991d5acad0a5f471f780210913273b682c
serglit <serglit72@gmail.com>
2015-03-09 23:21:58 -0700

    Added new class for test new contact

--- group.py
+++ group.py
@@ -4,3 +4,16 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
+
+class Groups:
+
+    def __init__(self, first_n, midd_n, last_n, nick_n, company, address, home_ph, cell_ph, email):
+        self.first_n=first_n
+        self.midd_n=midd_n
+        self.last_n=last_n
+        self.nick_n=nick_n
+        self.company=company
+        self.address=address
+        self.home_ph=home_ph
+        self.cell_ph=cell_ph
+        self.email=email
