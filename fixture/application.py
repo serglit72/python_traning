600e4c466f6791dcb0bfc41484f6ae7e39871970
serglit <serglit72@gmail.com>
2015-03-19 11:13:46 -0700

    Version 19.03.15 11.10

--- fixture/application.py
+++ fixture/application.py
@@ -2,30 +2,18 @@
 
 __author__ = 'Sergei'
 from selenium.webdriver.firefox.webdriver import WebDriver
-
+from fixture.session import SessionHelper
 class Application:
 
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-
+        self.session = SessionHelper(self)
     def open_home_page(self):
         # open home page
        wd = self.wd
        wd.get("http://localhost/addressbook/group.php")
 
-    def Login(self, username, password):
-        # login
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self):
         # open group page
@@ -54,10 +42,6 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def logout(self):
-        # logout
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 @ pytest.fixture
 def app(request):
@@ -10,13 +10,13 @@ def app(request):
     return fixture
 
     def test_test_add_group(app):
-        app.Login(username= "admin",password="secret")
+        app.session.Login(username= "admin",password="secret")
         app.create_fill_group_form(Group( name="Groupe4",header="Groun",footer="groupe"))
-        app.logout()
+        app.session.logout()
 
     def test_test_empty_group(app):
-        app.Login(username= "admin",password= "secret")
+        app.session.Login(username= "admin",password= "secret")
         app.create_fill_group_form(Group(name="",header= "",footer= ""))
-        app.logout()
+        app.session.logout()
 
 
