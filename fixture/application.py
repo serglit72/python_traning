e0d89c385dbb752c9dcfb9748699ff910b77f3e9
serglit <serglit72@gmail.com>
2015-03-24 14:38:27 -0700

    выделен вспомогательный класс session

--- fixture/application.py
+++ fixture/application.py
@@ -1,28 +1,19 @@
 
 __author__ = 'Sergei'
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -50,11 +41,6 @@ class Application:
         # return to groups page
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
-
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
 
     def destroy(self):
--- test/test_new_contact1.py
+++ test/test_new_contact1.py
@@ -13,14 +13,13 @@ def app(request):
 
 
 def test_test_new_contact(app):
-    success = True
     app.open_home_page()
-    app.login(username="admin",password= "secret")
+    app.session.login(username="admin",password= "secret")
     app.fill_form(Contact(first_n= "first",mid_n= "middle",last_n= "last",nick_n= "kuk",company= "adda",address= "575 oiweojdckjgsd,russia",home_ph= "12134519827",
                 cell_ph= "120092340980",email= "first.lastmiddle.@adda.com"))
     app.fill_drop_down_boxes()
     app.fill_end_form()
-    app.logout()
+    app.session.logout()
 
 
 
