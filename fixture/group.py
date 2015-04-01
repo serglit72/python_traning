9cf91b6a10d28e682b145ebebf347d007740bb88
serglit <serglit72@gmail.com>
2015-03-31 19:50:51 -0700

    Created new test for modify group

--- fixture/group.py
+++ fixture/group.py
@@ -9,31 +9,58 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+  #  def fill_group_form(self, group):
+   #     wd = self.app.wd
+   #     self.change_field_value("group_name", group.name)
+  #      self.change_field_value("group_header", group.header)
+   #     self.change_field_value("group_footer", group.footer)
+
+    def change_field_valueee(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_valueee("group_name", group.name)
+        self.change_field_valueee("group_header", group.header)
+        self.change_field_valueee("group_footer", group.footer)
+
+
     def create(self,group):
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+       # self.fill_group_form(group)
+        #self.fill_group_form(group)
         #submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def del_first_group(self):
-         wd = self.app.wd
-         self.open_groups_page()
-        #select group
-         wd.find_element_by_name("selected[]").click()
-        #delete group
-         wd.find_element_by_name("delete").click()
+    def select_first_groupp(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        self.select_first_groupp()
+
+    def del_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_groupp()
+        #delete group
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
 
     def return_to_group_page(self):
         # return to groups page
@@ -57,5 +84,14 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("wertwer")
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_groupp()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
