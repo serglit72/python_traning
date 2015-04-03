e533e900b92f0252347b5ad006e5217462adeebb
serglit <serglit72@gmail.com>
2015-04-02 21:55:57 -0700

    Created intellectual login &logout

--- fixture/application.py
+++ fixture/application.py
@@ -11,7 +11,7 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -26,7 +26,6 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
 
     def destroy(self):
         self.wd.quit()
--- fixture/group.py
+++ fixture/group.py
@@ -9,18 +9,6 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def change_field_value(self, field_name, text):
-        wd = self.app.wd
-        if text is not None:
-            wd.find_element_by_name(field_name).click()
-            wd.find_element_by_name(field_name).clear()
-            wd.find_element_by_name(field_name).send_keys(text)
-
-  #  def fill_group_form(self, group):
-   #     wd = self.app.wd
-   #     self.change_field_value("group_name", group.name)
-  #      self.change_field_value("group_header", group.header)
-   #     self.change_field_value("group_footer", group.footer)
 
     def change_field_valueee(self, field_name, text):
         wd = self.app.wd
@@ -36,13 +24,10 @@ class GroupHelper:
         self.change_field_valueee("group_footer", group.footer)
 
 
-    def create(self,group):
+    def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-       # self.fill_group_form(group)
-        #self.fill_group_form(group)
-        #submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
