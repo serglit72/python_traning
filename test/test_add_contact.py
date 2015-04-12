d16f3ab6ff4a493145741ffa91f911efc537b331
serglit <serglit72@gmail.com>
2015-04-11 18:03:56 -0700

    Upgrade assignment 10
    upgraded  3 tests about contacts

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,7 +2,6 @@ __author__ = 'Sergei'
 from model.contact import Contact
 
 def test_contact_new(app):
-    app.contact.create_c(Contact(first_n= "first",mid_n= "middle",last_n= "last",nick_n= "kuk",company= "adda",address= "575 oiweojdckjgsd,russia",home_ph= "12134519827",
-                cell_ph= "120092340980",email= "first.lastmiddle.@adda.com"))
-    app.contact.fill_boxes()
-    app.contact.fill_end_form()
+    app.contact.create_c(Contact(first_n= "first",mid_n= "middle",last_n= "last",nick_n= "kuk",company= "adda",address= "575 oiweojdckjgsd,russia", home_ph= "12134519827",
+    cell_ph= "120092340980", email= ""))
+
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,6 +6,4 @@ def test_contact_edit(app):
     if app.contact.count() == 0:
         app.contact.create_c(Contact(first_n= "first",mid_n= "middle",last_n= "last",nick_n= "kuk",company= "adda",address= "575 oiweojdckjgsd,russia",home_ph= "12134519827",
                 cell_ph= "120092340980",email= "first.lastmiddle.@adda.com"))
-        app.contact.fill_boxes()
-        app.contact.fill_end_form()
     app.contact.test_edit_contact()
